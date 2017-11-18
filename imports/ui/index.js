a1fa934888fd077f2e865e6ec0db75a2450909ef
Kevin Lin <kevin.lin.p@gmail.com>
2017-11-17 21:21:35 -0800

    basic polymer working with bower

---
+++ imports/ui/index.js
@@ -0,0 +1,10 @@
+// client/index.js
+//import '../imports/ui/components/test-element.html';
+  // include the webcomponents js file 
+import "./bower_components/webcomponentsjs/webcomponents-lite.js";
+
+//Remember to include a polymer component or polymer.html itself in any file
+
+import "./bower_components/polymer/polymer.html";
+
+import './test-element.html';

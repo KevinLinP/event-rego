301f24572f47539e784b2dacac60a6c5e603dd57
Kevin Lin <kevin.lin.p@gmail.com>
2017-12-07 11:09:39 -0800

    tweaking Accounts config

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -1,7 +1,5 @@
 import { Accounts } from 'meteor/accounts-base';
 
 Accounts.validateNewUser((user) => {
-  const emails = _.pluck(user.emails, 'address');
-
-  return _.contains(emails, 'sh3@wh3.org');
+  return false;
 });
